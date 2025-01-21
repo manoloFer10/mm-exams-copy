1d2eb9b0bdb305f5a092c6a350471e5da1dc978e
manoloFer10 <112580262+manoloFer10@users.noreply.github.com>
2025-01-21 11:02:59 -0300

    updated run command

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 export PYTHONPATH=$(pwd)
 
 python main.py \
+--num_samples '3' \
 --setting zero-shot \
---model qwen \
---selected_langs "['english']" \
+--model llama \
+--selected_langs ['slovak'] \
+--dataset dokato/multimodal-SK-exams \
+--api_key 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 \
