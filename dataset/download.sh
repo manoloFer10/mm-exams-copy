7a77300b719cc7836c07efd7c79ef07efb6e184a
Israfel Salazar <israfel.sr@gmail.com>
2025-01-23 13:45:16 -0300

    downloading

---
+++ dataset/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A EUHPC_D12_071
+#SBATCH --nodes=1
+#SBATCH -p boost_usr_prod
+#SBATCH --ntasks-per-node=4
+#SBATCH --job-name=CreateDataset
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=20GB
+#SBATCH --time=4:00:00
+
+python dataset/download.sh
