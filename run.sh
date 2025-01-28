e7c2b0a944095f9b4517e62f7148827c19c60155
Israfel Salazar <israfel.sr@gmail.com>
2025-01-27 21:42:52 -0300

    solving om

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -A EUHPC_D12_071
+#SBATCH -p boost_usr_prod
+#SBATCH --job-name=evaluation
+#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40G
+#SBATCH --time=4:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --output=./slurm/eval_%j.log
+
 export PYTHONPATH=$(pwd)
 
 python main.py \
